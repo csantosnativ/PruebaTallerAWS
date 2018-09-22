81963f756acfac268a8caf9246fea89052636271
Cristian Santos <csantos@nativapps.co>
2018-09-22 20:14:20 +0000

    NVM dir check

--- script/AfterInstall.sh
+++ script/AfterInstall.sh
@@ -2,4 +2,5 @@
 source /home/ec2-user/.bash_profile
 source /home/ec2-user/.bashrc
 cd /home/ec2-user/pruebaAWS/ 
+echo $NVM_DIR
 npm install 
--- script/ApplicationStart.sh
+++ script/ApplicationStart.sh
@@ -2,4 +2,5 @@
 source /home/ec2-user/.bash_profile
 source /home/ec2-user/.bashrc
 cd /home/ec2-user/pruebaAWS/
+echo $NVM_DIR
 node server.js > /dev/null 2> /dev/null < /dev/null &
