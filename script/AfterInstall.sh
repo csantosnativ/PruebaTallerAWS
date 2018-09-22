f31b8fb49d96503b85ba2462576a5c41f80819b0
Cristian Santos <csantos@nativapps.co>
2018-09-22 19:50:58 +0000

    AppSpec as root

--- script/AfterInstall.sh
+++ script/AfterInstall.sh
@@ -1,4 +1,2 @@
 #!/bin/bash 
-source /home/ec2-user/.bash_profile 
-source /home/ec2-user/.bashrc
 cd /home/ec2-user/pruebaAWS/ && npm install 
--- script/ApplicationStart.sh
+++ script/ApplicationStart.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-source /home/ec2-user/.bash_profile
-source /home/ec2-user/.bashrc
 cd /home/ec2-user/pruebaAWS/
 node server.js > /dev/null 2> /dev/null < /dev/null &
